a26733f2455489783469b64736e5b93f64b0c63f
PDungarshi <pchauhan@kenmarkitan.com>
2023-08-16 14:44:32 +0530

    small changes

--- lib/features/services/auth_service.dart
+++ lib/features/services/auth_service.dart
@@ -4,7 +4,6 @@ import 'dart:developer';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 import '../auth/presentation/pages/login_screen.dart';
 
